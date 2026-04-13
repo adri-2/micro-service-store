f91909f4ab451a11d931b001b3d5a250e1ce50bd
adri-2 <www.adriensani@gmail.com>
2026-04-13 16:56:41 +0100

    fix:serializer client create
    config: revue des configurations pour lancer docker

--- services/account_service/app/serializers.py
+++ services/account_service/app/serializers.py
@@ -10,8 +10,17 @@ User = get_user_model()
 class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
-        fields = ["id", "first_name","last_name", "created_at", "updated_at"]
-        read_only_fields = fields
+        fields = [
+            "id",
+            "first_name",
+            "last_name",
+            "email",
+            "phone_number",
+            "address",
+            "created_at",
+            "updated_at",
+        ]
+        read_only_fields = ["id", "created_at", "updated_at"]
         
 class ClientDetailSerializerService(serializers.ModelSerializer):
     class Meta:
--- services/account_service/core/settings.py
+++ services/account_service/core/settings.py
@@ -100,7 +100,7 @@ else:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
+            "NAME": BASE_DIR / "db13.sqlite3",
         }
     }
 
